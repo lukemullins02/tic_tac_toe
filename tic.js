a07db33d89ac7386a1dd4a6eb915297dd37c1a7f
= <lukejmullins@gmail.com>
2025-10-28 10:00:06 -0400

    Add logic changes to game

--- tic.js
+++ tic.js
@@ -6,7 +6,14 @@ const gameboard = (function () {
   ];
   const getBoard = () => board;
   const playerChoice = (x, y, type) => (board[x][y] = type);
-  return { getBoard, playerChoice };
+  const reset = () => {
+    board = [
+      [".", ".", "."],
+      [".", ".", "."],
+      [".", ".", "."],
+    ];
+  };
+  return { getBoard, playerChoice, reset };
 })();
 
 function player(name, type) {
@@ -23,6 +30,7 @@ const displayController = (function () {
   const createPlayer = (name) => {
     let playerName = prompt(`${name} enter name:`);
     let playerType;
+    console.log(playerName);
     if (name === "Player 1") {
       playerType = "X";
     } else {
@@ -37,7 +45,7 @@ const displayController = (function () {
     return currentBoard;
   };
 
-  const checkColumnWin = (type) => {
+  const checkColumnWin = (player) => {
     let arr = gameboard.getBoard();
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -45,21 +53,19 @@ const displayController = (function () {
         let y = j;
         let count = 0;
         for (; x < 3; x++) {
-          if (arr[x][y] === type) {
+          if (arr[x][y] === player.playerType) {
             count++;
           }
           if (count === 3) {
             console.log("win");
-            return true;
+            player.changePlayerScore();
           }
         }
       }
     }
-
-    return false;
   };
 
-  const checkRowWin = (type) => {
+  const checkRowWin = (player) => {
     let arr = gameboard.getBoard();
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -67,55 +73,48 @@ const displayController = (function () {
         let y = j;
         let count = 0;
         for (; y < 3; y++) {
-          if (arr[x][y] === type) {
+          if (arr[x][y] === player.playerType) {
             count++;
           }
           if (count === 3) {
             console.log("win");
-            return true;
+            player.changePlayerScore();
           }
         }
       }
     }
-    return false;
   };
 
-  const checkLeftDiagonalWin = (type) => {
-    console.log("hello");
+  const checkLeftDiagonalWin = (player) => {
     let arr = gameboard.getBoard();
     let j = 0;
     let count = 0;
     for (let i = 0; i < 3; i++) {
-      if (arr[i][j] === type) {
-        console.log(i, j);
+      if (arr[i][j] === player.playerType) {
         count++;
       }
       if (count === 3) {
         console.log("win");
-        return true;
+        player.changePlayerScore();
       }
       j++;
     }
-    return false;
   };
 
-  const checkRightDiagonalWin = (type) => {
+  const checkRightDiagonalWin = (player) => {
     let arr = gameboard.getBoard();
     let j = 2;
     let count = 0;
     for (let i = 2; i >= 0; i--) {
-      if (arr[i][j] === type) {
-        console.log(i, j);
+      if (arr[i][j] === player.playerType) {
         count++;
       }
       if (count === 3) {
         console.log("win");
-        return true;
+        player.changePlayerScore();
       }
       j--;
     }
-
-    return false;
   };
 
   return {
@@ -128,45 +127,54 @@ const displayController = (function () {
   };
 })();
 
-let player1 = displayController.createPlayer("Player 1");
-let player2 = displayController.createPlayer("Player 2");
-let currentBoard = displayController.createGameboard();
-let currentGame = false;
-let checkWin = false;
-let currentPlayer = 0;
+function playGame() {
+  let player1 = displayController.createPlayer("Player 1");
+  let player2 = displayController.createPlayer("Player 2");
+  let prevScore1 = 0;
+  let prevScore2 = 0;
+  let currentBoard = displayController.createGameboard();
+  let currentGame = true;
+  let currentPlayer = 0;
 
-console.log(player1, player2);
+  while (currentGame) {
+    if (currentPlayer === 0) {
+      let x = prompt(`${player1.playerName} please put x coordinate`);
+      let y = prompt(`${player1.playerName} please put y coordinate`);
+      currentBoard.playerChoice(x, y, player1.playerType);
+      currentPlayer = 1;
+    }
 
-while (currentGame !== true) {
-  if (currentPlayer === 0) {
-    let x = prompt(`${player1.playerName} please put x coordinate`);
-    let y = prompt(`${player1.playerName} please put y coordinate`);
-    currentBoard.playerChoice(x, y, player1.playerType);
-    currentPlayer = 1;
+    displayController.checkRowWin(player1);
+    displayController.checkColumnWin(player1);
+    displayController.checkLeftDiagonalWin(player1);
+    displayController.checkRightDiagonalWin(player1);
+
+    if (prevScore1 + 1 === player1.getPlayerScore()) {
+      currentBoard.reset();
+      prevScore1 === player1.getPlayerScore();
+      currentPlayer = 0;
+    }
+
+    if (currentPlayer === 1) {
+      let x = prompt(`${player2.playerName} please put x coordinate`);
+      let y = prompt(`${player2.playerName} please put y coordinate`);
+      currentBoard.playerChoice(x, y, player2.playerType);
+      currentPlayer = 0;
+    }
+
+    if (prevScore2 + 1 === player2.getPlayerScore()) {
+      currentBoard.reset();
+      prevScore2 === player2.getPlayerScore();
+      currentPlayer = 0;
+    }
+
+    displayController.checkRowWin(player2);
+    displayController.checkColumnWin(player2);
+    displayController.checkLeftDiagonalWin(player2);
+    displayController.checkRightDiagonalWin(player2);
     console.log(currentBoard.getBoard());
+    console.log(player2.getPlayerScore(), player1.getPlayerScore());
   }
-
-  currentGame = displayController.checkRowWin(player1.playerType);
-  currentGame = displayController.checkColumnWin(player1.playerType);
-  currentGame = displayController.checkLeftDiagonalWin(player1.playerType);
-  currentGame = displayController.checkRightDiagonalWin(player1.playerType);
-
-  if (currentPlayer === 1) {
-    let x = prompt(`${player2.playerName} please put x coordinate`);
-    let y = prompt(`${player2.playerName} please put y coordinate`);
-    currentBoard.playerChoice(x, y, player2.playerType);
-    currentPlayer = 0;
-  }
-
-  currentGame = displayController.checkRowWin(player2.playerType);
-  currentGame = displayController.checkColumnWin(player2.playerType);
-  currentGame = displayController.checkLeftDiagonalWin(player2.playerType);
-  currentGame = displayController.checkRightDiagonalWin(player2.playerType);
 }
 
-console.log(currentGame);
-
-displayController.checkRowWin("x");
-displayController.checkColumnWin("x");
-displayController.checkLeftDiagonalWin("x");
-displayController.checkRightDiagonalWin("x");
+playGame();
